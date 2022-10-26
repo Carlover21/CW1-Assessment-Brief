ebc4604e1044d9e74a21a702836f99b9169aca8d
Carlover21 <82425897+Carlover21@users.noreply.github.com>
2022-10-26 18:28:03 +0100

    update

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
-public class Main 
+public class Main
 {
-    public static void main(String[] args)
+     public static void main(String[] args)
+     
     {
-        System.out.println ("Rayan's CW1");
+        System.out.println ("Rayan's CW1 ");
+        System.out.println("Hello user :)");
+        String songs = InputReader.getString("Would you like to listen to music yes/no ? :" );
     }
 }
---
+++ Songs.java
@@ -0,0 +1,23 @@
+public class Songs {
+    private String songs;
+    private String artist;
+    private int playcount;
+
+    //Constructor
+    public Songs(String songs, String artist, int playcount)
+    {
+         this.songs= songs;
+         this.artist= artist;
+         this.playcount= playcount;
+    }
+    
+    //print method
+    public void print()
+    {
+       System.out.print("SONG:"+ this.songs + "\t");
+       System.out.print("ARTIST: "+this.artist + "\t");
+       System.out.print("PLAYCOUNT: "+this.playcount + "\t");
+    }
+
+
+}
