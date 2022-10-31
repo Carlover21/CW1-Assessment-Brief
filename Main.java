44ba7540c5b435267667b53499a1c32538f229a7
Carlover21 <82425897+Carlover21@users.noreply.github.com>
2022-10-31 18:43:46 +0000

    update + fixing all bugs

--- Main.java
+++ Main.java
@@ -1,55 +1,51 @@
+import java.util.ArrayList;
+
 public class Main
 {
-     public static void main(String[] args)
-     
+    String songs = Main.main(String[]);
+    public static void main(String[] args)
     {
         System.out.println ("Rayan's CW1 ");
         System.out.println("Hello user :)");
-        String songs = InputReader.getString("Would you like to listen to music yes/no ? :" );
+        String Songs = InputReader.getString("Would you like to listen to music yes/no ? :" );
 
 
+
+    
         Songs celine3x = new Songs("CELINE 3x", "GAZO", 26612713);
-        celine3x.print();
-
         Songs mauvaisx2  = new Songs("MAUVAIS 2X", "GAZO x NINHO", 25218394);
-        mauvaisx2.print();
-
         Songs tdb = new Songs("TDB", "OBOY", 59837163);
-        tdb.print();
-
         Songs balader = new Songs("Balader", "Soolking feat Niska", 56068714);
-        balader.print();
-
         Songs attentat = new Songs("Attentat", "PLK", 13116061);
-        attentat.print();
-
         Songs bandeorganisée = new Songs("Bande Organisée", "Sch / Kofs / Jul / Naps / Soso maness / Elams / Solda / Houari ", 424525472);
-        bandeorganisée.print();
-
         Songs gangstasparadice = new Songs("Gansta's Paradice", "Coolio", 1098971655);
-        gangstasparadice.print();
-        /* 
-        Songs gazo = new Songs("CELINE 3x", "GAZO", 26612713);
-        gazo.print();
+        Songs puffinonzootiez  = new Songs( "PUFFIN ON ZOOTIEZ","Future"  , 17502707);
+        Songs luciddreams = new Songs("Lucid Dreams", "Juice WRLD", 849411712);
+        Songs memyself = new Songs("Me, Myself & I","G-Eazy x Bebe Rexha" , 573436155);
+        Songs oneup = new Songs("One Up", "Central Cee", 423611);
+        Songs ferrari = new Songs("Ferrari", "Pop Smoke ft. Central Cee, Tion Wayne & Russ Million", 7277178);
+        Songs lif = new Songs("LIF", "SCH", 1609166);
+        Songs b3ida = new Songs("B3ida", "Flenn", 15630088);
+      
 
-        Songs gazo = new Songs("CELINE 3x", "GAZO", 26612713);
-        gazo.print();
+        
+         ArrayList<Songs> songs = new ArrayList<Songs>();
+         songs.add(celine3x);
+         songs.add(mauvaisx2);
+         songs.add(tdb);
+         songs.add(balader);
+         songs.add(attentat);
+         songs.add(bandeorganisée);
+         songs.add(gangstasparadice);
+         songs.add(puffinonzootiez);
+         songs.add(luciddreams);
+         songs.add(memyself);
+         songs.add(oneup);
+         songs.add(ferrari);
+         songs.add(lif);
+         songs.add(b3ida);
+        
 
-        Songs gazo = new Songs("CELINE 3x", "GAZO", 26612713);
-        gazo.print();
-
-        Songs gazo = new Songs("CELINE 3x", "GAZO", 26612713);
-        gazo.print();
-
-        Songs gazo = new Songs("CELINE 3x", "GAZO", 26612713);
-        gazo.print();
-
-        Songs gazo = new Songs("CELINE 3x", "GAZO", 26612713);
-        gazo.print();
-
-        Songs gazo = new Songs("CELINE 3x", "GAZO", 26612713);
-        gazo.print();
-    */
 
     }
 }
--- Songs.java
+++ Songs.java
@@ -18,6 +18,6 @@ public class Songs {
        System.out.print("ARTIST: "+this.artist + "\t");
        System.out.println("PLAYCOUNT: "+this.playcount + "\t");
     }
-
+    
 
 }
