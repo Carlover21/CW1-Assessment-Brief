add456c96f9482f833f6410c3eba2285c6bcdecb
Carlover21 <82425897+Carlover21@users.noreply.github.com>
2022-11-05 23:41:37 +0000

    Update+ added some new commands

--- Main.java
+++ Main.java
@@ -11,11 +11,7 @@ public class Main
         String q1 = InputReader.getString("Would you like to listen to music  yes/no ?>");
 
          if (q1.equalsIgnoreCase("yes") ) {
-      System.out.println("this is the list of songs");
-     
-
-
-    
+        System.out.println("this is the list of songs");
         Songs celine3x = new Songs("CELINE 3x", "GAZO", 26612713);
         Songs mauvaisx2  = new Songs("MAUVAIS 2X", "GAZO x NINHO", 25218394);
         Songs tdb = new Songs("TDB", "OBOY", 59837163);
@@ -31,9 +27,7 @@ public class Main
         Songs lif = new Songs("LIF", "SCH", 1609166);
         Songs b3ida = new Songs("B3ida", "Flenn", 15630088);
       
-        
-        
-         ArrayList<Songs> songs = new ArrayList<Songs>();
+        ArrayList<Songs> songs = new ArrayList<Songs>();
          songs.add(celine3x);
          songs.add(mauvaisx2);
          songs.add(tdb);
@@ -56,6 +50,8 @@ int id = 1;
             id++;
          }
          String q2 = InputReader.getString("would you like to add a song ? >");
+         if (q2.equalsIgnoreCase("yes") ) {
+          
     String songsadd = InputReader.getString("Enter the name of the song : >");
     String artistadd = InputReader.getString("Enter the name of the artist : >");
     int playcountadd = InputReader.getInt("Enter the playcount : >"); 
@@ -67,26 +63,53 @@ int id = 1;
     for (Songs i : songs) {
       i.print();
     }
-
-    int q3 =InputReader.getInt("Would you like to remove a song ? (choose the songs in order from 0 to 14)>");
-    String q4 = InputReader.getString("are you sure you want to remove song number " + q3 + "? >");
-    if (q4.equalsIgnoreCase("yes") ) {
-
     
+  }
+  else {
+    System.out.println("okey");
+}
+  
 
-    songs.remove(q3);
+    String q3 =InputReader.getString("Would you like to remove a song yes/no ? ");
+    if (q3.equalsIgnoreCase("yes") ) {
+    
+    int q4 =InputReader.getInt("Choose the songs in order from 0 to 14>");
+    if ( q4 <= 14  ) {
+    String q5 = InputReader.getString("Are you sure you want to remove song number " + q4 + "? >");
+    if (q5.equalsIgnoreCase("yes") ) {
+    
+    songs.remove(q4);
     for (Songs i : songs) {
       System.out.print("ID: 0 ");
       i.print();
-    }
-   }
-    
-
-         }
-         else {
-            System.out.println("Good bye.");
-    }
-    
+                          }
+                        }
+                        }
+  else {
+    System.out.println("Try to submit a number from  0 to 14");
+    int q8 =InputReader.getInt("Choose the songs in order from 0 to 14>");
+    if ( q8 <= 14  ) {
+      String q9 = InputReader.getString("Are you sure you want to remove song number " + q8 + "? >");
+      if (q9.equalsIgnoreCase("yes") ) {
+      }
+      songs.remove(q8);
+      for (Songs i : songs) {
+        System.out.print("ID: 0 ");
+        i.print();
+}
+}
 }
 
-} 
+    
+System.out.println("Thanks for using the program good bye :)");
+    }
+
+
+}
+else {
+  System.out.println("Good bye.");
+}
+
+}
+
+}
